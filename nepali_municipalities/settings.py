e668eea9a6bd0b91d290c3de373f0e2436eaa8c3
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2022-01-02 18:08:32 +0545

    feat: changed deployment setting

--- nepali_municipalities/settings.py
+++ nepali_municipalities/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
+import django
 from dotenv import load_dotenv
 import os
 
@@ -112,7 +114,7 @@ DATABASES = {
 
     'default': {
 
-        'ENGINE': env('DJANGO_DATABASE_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
         'NAME': env('DJANGO_DATABASE_NAME'),
 
