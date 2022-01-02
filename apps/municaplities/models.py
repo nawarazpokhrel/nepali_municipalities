71f8b8fa00abfc38701bf2b30bddd2c8c83842dd
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2022-01-02 21:27:13 +0545

    feat: alter id field in models.py

--- apps/municaplities/models.py
+++ apps/municaplities/models.py
@@ -8,12 +8,12 @@ from django_extensions.db.fields import ShortUUIDField
 
 
 class Province(models.Model):
-    id = ShortUUIDField(
-        primary_key=True,
-        auto=True,
-        editable=False,
-        unique=True,
-    )
+    # id = ShortUUIDField(
+    #     primary_key=True,
+    #     auto=True,
+    #     editable=False,
+    #     unique=True,
+    # )
     country = models.CharField(default='Nepal',max_length=10)
     name = models.CharField(max_length=50)
 
@@ -22,12 +22,12 @@ class Province(models.Model):
 
 
 class District(models.Model):
-    id = ShortUUIDField(
-        primary_key=True,
-        auto=True,
-        editable=False,
-        unique=True,
-    )
+    # id = ShortUUIDField(
+    #     primary_key=True,
+    #     auto=True,
+    #     editable=False,
+    #     unique=True,
+    # )
     province = models.ForeignKey(to=Province, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
@@ -36,12 +36,12 @@ class District(models.Model):
 
 
 class Municipalities(models.Model):
-    id = ShortUUIDField(
-        primary_key=True,
-        auto=True,
-        editable=False,
-        unique=True,
-    )
+    # id = ShortUUIDField(
+    #     primary_key=True,
+    #     auto=True,
+    #     editable=False,
+    #     unique=True,
+    # )
     district = models.ForeignKey(to=District, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
